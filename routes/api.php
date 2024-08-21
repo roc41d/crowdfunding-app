f03f08efffc918a5409c549118a80f50907787a0
Rocard Fonji <fonkengrocard@gmail.com>
2024-08-21 13:32:02 +0100

    chore: clean up route file

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\DonationController;
-use App\Http\Controllers\DonationTransactionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -13,6 +12,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/donations', [DonationController::class, 'createDonation']);
     Route::get('/donations/{id}', [DonationController::class, 'getDonation']);
     Route::post('/donations/{id}/donate', [DonationController::class, 'donate']);
-
-    Route::post('/donation-transactions', [DonationTransactionController::class, 'donate']);
 });
